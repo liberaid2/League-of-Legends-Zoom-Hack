8524e0281011bddaf3003e2ea6810f1f516c5ae9
Egor Shitov <red-fox_ddrace@mail.ru>
2016-08-11 01:49:03 +0300

    Working zoomhack path 6.16

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,23 @@
 #include "MemoryControlLib.h"
 
 int main(){
+	MemoryControlLib *mcLib = new MemoryControlLib("League of Legends.exe");
+	mcLib->Open();
 	
+	MODULEENTRY32 game = {0};
+	mcLib->GetModuleInfo("", &game, true);
 	
+	DWORD sign = mcLib->FindSignature((DWORD)game.modBaseAddr, game.modBaseSize, (PBYTE)"\x0F\x2F\xC8\x8B\x86\x00\x00\x00\x00\xF3\x0F\x10\x50\x24\x77\x0D\x0F\x2F\xD1\x76\x05\x0F\x28\xC2\xEB\x03\x0F\x28\xC1", "xxxxx????xxxxxxxxxxxxxxxxxxxx");
+	
+	printf("Found signature: 0x%X\n", sign);
+	
+	if(sign)
+	{
+		byte nopBuffer[] = {0x90, 0x90, 0x90};
+		mcLib->Write(nopBuffer, sign, 3);
+	}
+	
+	mcLib->Close();
 	system("PAUSE >nul");
 	return 0;
 }
